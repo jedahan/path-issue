575e6df13e550b3cabf45c91bef69b6f6cd11f61
Jonathan Dahan <jonathan@jonathan.is>
2015-03-17 17:27:40 -0400

    minimal example

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,7 +1,5 @@
 #include "ofApp.h"
 
-#define MS_FOR_SELECTION 5000
-
 void ofApp::setup(){
   int w = ofGetViewportWidth();
   int h = ofGetViewportHeight();
@@ -10,10 +8,6 @@ void ofApp::setup(){
   circles.push_back(Circle(ofColor(0,255,255), 2 * w/6, h/2, 100));
   circles.push_back(Circle(ofColor(255,0,255), 3 * w/6, h/2, 100));
   circles.push_back(Circle(ofColor(255,255,0), 4 * w/6, h/2, 100));
-  selectedColor = circles[0].c;
-  ofBackground(0);
-  ofGetWindowPtr()->setWindowTitle("fireworks");
-  startTime = endTime = -1;
   line.setStrokeWidth(1);
   line.setCircleResolution(180);
 }
@@ -30,10 +24,6 @@ void ofApp::update(){
       line.arc(p,r,r,0,360*pct);
     }
   }
-  if(endTime > 0 && ofGetElapsedTimeMillis() >= endTime){
-    ofMessage msg(ofToString("selected"));
-    ofSendMessage(msg);
-  }
 }
 
 void ofApp::draw(){
@@ -43,33 +33,6 @@ void ofApp::draw(){
     ofSetColor(c.c);
     ofDrawCircle(c.x,c.y,c.r,c.r);
   }
-  ofSetColor(selectedColor);
-  ofDrawRectangle(mouseX-10,mouseY-10,20,20);
- 
-  for(ofMesh firework : fireworks){
-    firework.drawVertices();
-  }
-}
-
-void ofApp::keyPressed(int key){
-
-}
-
-void ofApp::keyReleased(int key){
-
-}
-
-void ofApp::mouseMoved(int x, int y){
-  if(whichCircle() && startTime < 0) {
-    startTime = ofGetElapsedTimeMillis();
-    endTime = startTime + MS_FOR_SELECTION;
-  }
-  if(! whichCircle() && endTime > 0){
-    startTime = endTime = -1;
-  }
-}
-
-void ofApp::mouseDragged(int x, int y, int button){
 }
 
 Circle * ofApp::whichCircle() {
@@ -87,25 +50,6 @@ bool ofApp::pointInCircle(int pX, int pY, Circle c) {
     return dx*dx + dy*dy <= c.r*c.r;
 }
 
-void ofApp::explode(int x, int y){
-  ofMesh firework;
-  for(int i=0; i<1000; i++){
-    firework.addVertex(ofVec3f(x+(ofRandom(80)-40),(y+ofRandom(80)-40),0));
-    firework.addColor(ofColor(selectedColor));
-  }
-  fireworks.push_back(firework);
-}
-
-void ofApp::mousePressed(int x, int y, int button){
-  if (!whichCircle()) {
-    explode(x,y);
-  }
-}
-
-
-void ofApp::mouseReleased(int x, int y, int button){
-}
-
 void ofApp::windowResized(int w, int h){
    circles[0].x = 2 * w/6;
    circles[0].y = h/2;
@@ -113,12 +57,4 @@ void ofApp::windowResized(int w, int h){
    circles[1].y = h/2;
    circles[2].x = 4 * w/6;
    circles[2].y = h/2;
-}
-
-void ofApp::gotMessage(ofMessage msg) {
-  selectedColor = whichCircle()->c;
-}
-
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -11,26 +11,13 @@ class ofApp : public ofBaseApp{
 		void update();
 		void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
 		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
 		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-    void explode(int x, int y);
 		
-    vector<Circle> circles;
     bool pointInCircle(int pX, int pY, Circle c);
-    Circle * whichCircle();
-    ofColor selectedColor;
-    ofShader shader, explodeshader;
-    ofPath line;
-    vector<ofMesh> fireworks;
 
-    bool select;
-    float startTime;
-    float endTime;
+    vector<Circle> circles;
+    Circle * whichCircle();
+    ofPath line;
+
 };
